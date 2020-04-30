32998d609d71676bc26a3e5bf3cdd3a362c299fc
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2020-04-30 15:55:08 +0800

    Add local storage API

--- background.js
+++ background.js
@@ -1,173 +1,118 @@
-var clicks = 0;
-var anchor = document.createElement('a');
-var bucket = [];
-var record = true;
-var hostname;
+const requests = browser.webRequest.onHeadersReceived,
+anchor = document.createElement('a'),
+storage = browser.storage.local;
+let status = true;
 
-browser.webRequest.onHeadersReceived.addListener(
-    recordHttpResponse, {
-        urls: ["<all_urls>"]
-    },
-    ["responseHeaders"]
-);
+listener();
+browser.runtime.onMessage.addListener(fromPopup);
 
-function recordHttpResponse(response) {
-    if (record) {
-        if (response.statusCode != 404){
-            response.responseHeaders.forEach(function(header) {
-                if (header.name.toLowerCase() == "x-amz-request-id") {
-                    anchor.href = response.url;
-                    hostname = anchor.hostname;
-                    if (hostname == "s3.amazonaws.com"){
-                        hostname += "/"+anchor.pathname.split("/")[1];
-                    }
-                    var bucketNone = bucket.filter(bucket => bucket.bucketName == hostname) == "";
-                    var noFavicon = !anchor.pathname.includes("favicon.ico");
-                    if (bucketNone && noFavicon) {
-                        var req = new XMLHttpRequest();
-                        bucket.push({
-                            "bucketName": hostname,
-                            "bucketPermissions": []
-                        });
-                        var bucketFilter = bucket.filter(bucket => bucket.bucketName == hostname);
-                        var bucketPermissions = bucketFilter[0].bucketPermissions;
-                        req.open("GET", "https://"+hostname+"/?acl", true);
-                        req.addEventListener("load", function() {
-                            if (req.responseXML == null) {
-                                bucketPermissions.push({
-                                    "title": "Error",
-                                    "permission": ["Error getting permissions"]
-                                });
-                            } else {
-                                var xml = req.responseXML.getElementsByTagName("URI");
-                                if (xml.length == 0){
-                                    xml = req.responseXML.getElementsByTagName("Code");
-                                    if(typeof(xml[0]) == "undefined"){
-                                        bucketPermissions.push({
-                                            "title": "Error",
-                                            "permission": ["Error getting permissions"]
-                                        });
-                                    } else {
-                                        bucketPermissions.push({
-                                            "title": xml[0].childNodes[0].nodeValue,
-                                            "permission": [xml[0].nextElementSibling.childNodes[0].nodeValue]
-                                        });
-                                    }
-                                } else {
-                                    var permissionFilter;
-                                    for (var i = 0; i < xml.length; i++){
-                                        var text = xml[i].childNodes[0].nodeValue;
-                                        var permission = xml[i].parentNode.nextElementSibling.childNodes[0].nodeValue;
-                                        if (text.includes("AllUsers")){
-                                            permissionFilter = bucketPermissions.filter(bucket => bucket.title == "All Users");
-                                            if (permissionFilter.length == 0) {
-                                                bucketPermissions.push({
-                                                    "title": "All Users",
-                                                    "permission": [permission]
-                                                });
-                                            } else {
-                                                permissionFilter[0].permission.push(permission);
-                                            }
-                                        } else if (text.includes("AuthenticatedUsers")){
-                                            permissionFilter = bucketPermissions.filter(bucket => bucket.title == "Authenticated");
-                                            if (permissionFilter.length == 0) {
-                                                bucketPermissions.push({
-                                                    "title": "Authenticated",
-                                                    "permission": [permission]
-                                                });
-                                            } else {
-                                                permissionFilter[0].permission.push(permission);
-                                            }
-                                        } else if (text.includes("LogDelivery")){
-                                            permissionFilter = bucketPermissions.filter(bucket => bucket.title == "Log Delivery");
-                                            if (permissionFilter.length == 0) {
-                                                bucketPermissions.push({
-                                                    "title": "Log Delivery",
-                                                    "permission": [permission]
-                                                });
-                                            } else {
-                                                permissionFilter[0].permission.push(permission);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        });
-                        req.send();
-                        addNumber();
-                    }
-                }
-            });
-        }
-    }
-    return {
-        responseHeaders: response.responseHeaders
-    };
+function listener() {
+    requests.addListener(
+        record, {
+            urls: ["<all_urls>"]
+        },
+        ["responseHeaders"]
+    );
 }
 
-function addNumber() {
+function fromPopup(toRecord,sender,sendResponse) {
+    if (toRecord) {
+        if (requests.hasListener(record)){
+            requests.removeListener(record);
+            status = false;
+        }
+        else {
+            listener();
+            status = true;
+        }
+    } else {
+        sendResponse({
+            record: status
+        })
+    }
+}
+
+async function record(response) {
+    const headers = response.responseHeaders,
+    s3 = headers.filter(header => header.name == "x-amz-request-id")[0];
+    if (headers.indexOf(s3) != -1) {
+        let hostname;
+        const stored = await storage.get(),
+        buckets = Object.keys(stored);
+        anchor.href = response.url;
+        hostname = anchor.hostname;
+        if (hostname == "s3.amazonaws.com") {
+            hostname += "/"+anchor.pathname.split("/")[1];
+        }
+        let noFavicon = !hostname.includes("favicon.ico");
+        if (buckets.indexOf(hostname) == -1 && noFavicon) {
+            storage.set({
+                [hostname]: {}
+            });
+            let infos = await info(hostname);
+            await getPerms(infos, hostname);
+            await addNumber();
+        }
+    }
+}
+
+async function info(hostname) {
+    let response = await fetch("http://"+hostname+"/?acl"), 
+    text = await response.text(),
+    parser = new DOMParser(),
+    xml = parser.parseFromString(text,"text/xml");
+    return xml
+}
+
+async function getPerms(xml, hostname) {
+    const hasUri = xml.getElementsByTagName("URI"),
+    hasCode = xml.getElementsByTagName("Code"),
+    bucket = await storage.get(hostname),
+    permissions = bucket[hostname];
+    if (!hasUri[0] && hasCode[0]) {
+        let title = hasCode[0].childNodes[0].nodeValue,
+        perm = hasCode[0].nextElementSibling.childNodes[0].nodeValue;
+        permissions[title] = [perm];
+    } else if (hasUri[0] && !hasCode[0]){
+        for (let i = 0; i < hasUri.length; i++){
+            let text = hasUri[i].childNodes[0].nodeValue,
+            perm = hasUri[i].parentNode.nextElementSibling
+                   .childNodes[0].nodeValue;
+            if (text.includes("AllUsers")){
+                if (permissions["All Users"]){
+                    permissions["All Users"].push(perm);
+                } else {
+                    permissions["All Users"] = [perm]
+                }
+            } else if (text.includes("AuthenticatedUsers")){
+                if (permissions["Authenticated"]){
+                    permissions["Authenticated"].push(perm);
+                } else {
+                    permissions["Authenticated"] = [perm]
+                }
+            } else if (text.includes("LogDelivery")){
+                if (permissions["Log Delivery"]){
+                    permissions["Log Delivery"].push(perm);
+                } else {
+                    permissions["Log Delivery"] = [perm];
+                }
+            }
+        }
+    } else {
+        permissions.Error = ["Error getting permissions"];
+    }
+    storage.set({
+        [hostname]: permissions
+    });
+}
+
+async function addNumber() {
+    let buckets = await storage.get();
+    buckets = Object.keys(buckets);
     browser.browserAction.setBadgeText({
-        text: (++clicks).toString()
+        text: (buckets.length).toString()
     });
     browser.browserAction.setBadgeBackgroundColor({
         color: "green"
     });
 }
-
-browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action == "change") {
-        if (record) {
-            browser.browserAction.setBadgeText({
-                text: "!"
-            });
-            browser.browserAction.setBadgeBackgroundColor({
-                color: "orange"
-            });
-            record = false;
-        } else {
-            if (clicks == 0){
-                browser.browserAction.setBadgeText({
-                    text: null
-                });
-            } else {
-                browser.browserAction.setBadgeText({
-                    text: clicks.toString()
-                });
-            }
-            browser.browserAction.setBadgeBackgroundColor({
-                color: "green"
-            });
-            record = true;
-        }
-    }
-    if (request.action == "clear") {
-        bucket = [];
-        browser.browserAction.setBadgeText({
-            text: null
-        });
-        clicks = 0;
-    }
-    if (request.action == "check") { 
-        if (bucket != "") {
-            var blob = new Blob([JSON.stringify(bucket, null, 2)], {type: 'text/plain'});
-            blob = window.URL.createObjectURL(blob);
-        }
-        sendResponse({
-            "recordStatus": record,
-            "bucketList": bucket,
-            "file": blob
-        });
-    }
-    if (request.action == "delete") {
-        browser.browserAction.setBadgeText({
-            text: (--clicks).toString()
-        });
-        if (clicks == 0) {
-            browser.browserAction.setBadgeText({
-                text: null
-            });
-        }
-        var index = bucket.indexOf(bucket.filter(bucket => bucket.bucketName == request.buckets)[0]);
-        bucket.splice(index, 1);
-    }
-});
--- popup/popup.js
+++ popup/popup.js
@@ -1,78 +1,110 @@
-document.getElementById("record").addEventListener("change", () => {
-	var sending = browser.runtime.sendMessage({
-    	"action" : "change"
-  	});
-});
+const record = document.getElementById("record"),
+clear = document.getElementById("clear"),
+save = document.getElementById("save"),
+content = document.getElementById("content");
+storage = browser.storage.local;
 
-document.getElementById("clear").addEventListener("click", () => {
-	var sending = browser.runtime.sendMessage({
-    	"action" : "clear"
-  	});
-});
+record.addEventListener("change", recordBucket);
+clear.addEventListener("click", clearBucket);
 
-(() => {
-	var sending = browser.runtime.sendMessage({
-    	"action": "check"
-  	});
-  	sending.then((response) => {
-  		var save = document.getElementById("save");
-  		var record = document.getElementById("record");
-  		if (response.recordStatus) { 
-  			record.checked = true;
-  		} else {
-  			record.checked = false;
-  		}
-  		const parser = new DOMParser();
-  		for (var i=0; i<response.bucketList.length; i++){
-  			var bucket = response.bucketList[i];
-  			var permissions = bucket.bucketPermissions
-  			var html = `<div class="bucket" style="cursor: pointer;"><i class="down"></i><div class="bucketName"><p>`+bucket.bucketName
-  			+`</p></div><a href="" class="bucketDelete"><div>Delete</div></a></div><div class="bucketContent">`;
-			for (var bucketPermission of permissions) {
-				html += "<p><b>"+bucketPermission.title+"</b><br>"+bucketPermission.permission+"</p>";
-			}
-  			html += "</div>";
-			var parsed = parser.parseFromString(html, `text/html`);
-			var tags = parsed.getElementsByTagName(`body`);
-			for (var tag of tags) {
-				document.getElementById("content").appendChild(tag); 
-			}
-  		}
-  		if (response.file != null){
-  			save.download = "buckets.txt";
-  			save.href = response.file;
-  		} else {
-  			save.removeAttribute("download"); 
-  		}
-  		var bucket = document.getElementsByClassName("bucket");
-  		var bucketDelete = document.getElementsByClassName("bucketDelete");
-		var bucketName = document.getElementsByClassName("bucketName");
-		for (var i = 0; i < bucketDelete.length; i++) {
-			bucketDelete[i].addEventListener('click', function() {
-				var sending = browser.runtime.sendMessage({
-			    	"action" : "delete",
-			    	"buckets": this.bucket.textContent
-			  	});
-			});
-			bucketDelete[i].bucket = bucketName[i];
-			bucket[i].onmouseover = function () {
-				bucketDelete[this.num].style.visibility = "visible";
-			}
-			bucket[i].onmouseout = function () {
-				bucketDelete[this.num].style.visibility = "hidden";
-			}
-			bucket[i].num = i;
-			bucket[i].addEventListener('click', function() {
-				var content = this.nextElementSibling;
-				this.classList.toggle("active");
-			 	if (content.style.maxHeight){
-			      	content.style.maxHeight = null;
-			      	this.childNodes[0].className = "down";
-			    } else {
-			    	this.childNodes[0].className = "up";
-			      	content.style.maxHeight = content.scrollHeight + "px";
-			    } 
-			});
-		}
-  	}); 
+(async function () {
+	let status = await getRecordStatus();
+	await generateSave();
+	await displayBucket();
+	displayContent();
+	if (status) {
+		record.checked = true;
+	} else {
+		record.checked = false;
+	}
 })();
+
+async function recordBucket() {
+	await browser.runtime.sendMessage(true);
+}
+
+async function clearBucket() {
+	await browser.storage.local.clear();
+	browser.browserAction.setBadgeText({
+        text: null
+    });
+}
+
+async function getRecordStatus() {
+	let response = await browser.runtime.sendMessage(false);
+	return response.record;
+}
+
+async function generateSave() {
+	const buckets = await storage.get(),
+	noOfBuckets = Object.keys(buckets);
+	if (noOfBuckets.length != 0) {
+		let blob = new Blob(
+			[JSON.stringify(buckets, null, 2)],
+			 {type: 'text/plain'}
+		);
+		blob = window.URL.createObjectURL(blob);
+		save.download = "buckets.txt";
+		save.href = blob; 
+	} else {
+		save.removeAttribute("download");
+	}
+}
+
+async function displayBucket() {
+	const buckets = await storage.get(),
+	noOfBuckets = Object.keys(buckets),
+	parser = new DOMParser;
+	if (noOfBuckets.length != 0) {
+		for (let bucket of noOfBuckets) {
+			let html = `<div class="bucket" style="cursor: pointer;">
+			<i class="down"></i><div class="bucketName"><p>`+bucket+`</p></div>
+			<a href="" class="bucketDelete"><div>Delete</div></a></div>
+			<div class="bucketContent">`;
+			let perms = buckets[bucket],
+			permsName = Object.keys(perms);
+			for (let perm of permsName) {
+				html += "<p><b>"+perm+"</b><br>"+perms[perm]+"</p>";
+			}
+			html += "</div>";
+			let parsed = parser.parseFromString(html, "text/html"),
+			tags = parsed.getElementsByTagName("body")[0];
+			document.getElementById("content").appendChild(tags); 
+		}
+	}
+
+}
+
+async function displayContent() {
+	const bucket = document.getElementsByClassName("bucket"),
+	bucketDelete = document.getElementsByClassName("bucketDelete"),
+	bucketName = document.getElementsByClassName("bucketName");
+	for (var i = 0; i < bucketDelete.length; i++) {
+		bucketDelete[i].addEventListener('click', deleteBucket);
+		bucketDelete[i].bucket = bucketName[i];
+		bucket[i].onmouseover = function () {
+			bucketDelete[this.num].style.visibility = "visible";
+		}
+		bucket[i].onmouseout = function () {
+			bucketDelete[this.num].style.visibility = "hidden";
+		}
+		bucket[i].num = i;
+		bucket[i].addEventListener('click', openContent);
+	}
+}
+
+async function deleteBucket() {
+	await storage.remove(this.bucket.textContent);
+}
+
+function openContent() {
+	var content = this.nextElementSibling;
+	this.classList.toggle("active");
+ 	if (content.style.maxHeight){
+      	content.style.maxHeight = null;
+      	this.childNodes[0].className = "down";
+    } else {
+    	this.childNodes[0].className = "up";
+      	content.style.maxHeight = content.scrollHeight + "px";
+    } 
+}
