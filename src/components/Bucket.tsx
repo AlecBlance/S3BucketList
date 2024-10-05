405d3a5ea3c75548afa180c90128573236e12eda
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2024-10-05 11:51:08 +0800

    refactor: :recycle: not accordion content if no permissions (error tab)

--- src/components/Bucket.tsx
+++ src/components/Bucket.tsx
@@ -7,6 +7,7 @@ import {
 import { IBucketInfo } from "@/types";
 
 const Bucket = ({ info }: { info: IBucketInfo }) => {
+  const isPermPresent = !!Object.keys(info.permissions).length;
   return (
     <Accordion
       type="single"
@@ -14,23 +15,28 @@ const Bucket = ({ info }: { info: IBucketInfo }) => {
       className="first:rounded-tl-md first:rounded-tr-md"
     >
       <AccordionItem value="item-1">
-        <AccordionTrigger className="bg-card p-2 text-sm hover:no-underline">
+        <AccordionTrigger
+          className="bg-card p-2 text-sm hover:no-underline"
+          isPermPresent={isPermPresent}
+        >
           {info.hostname}
         </AccordionTrigger>
-        <AccordionContent className="bg-secondary p-2 text-xs">
-          <div className="flex flex-col gap-y-2">
-            {Object.entries(info.permissions).map(([key, value]) => (
-              <div key={key} className="flex flex-col gap-y-1">
-                <p className="font-bold">{key}</p>
-                <ul className="list-inside list-disc">
-                  {value.map((perm) => (
-                    <li key={perm}>{perm}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-        </AccordionContent>
+        {isPermPresent && (
+          <AccordionContent className="bg-secondary p-2 text-xs">
+            <div className="flex flex-col gap-y-2">
+              {Object.entries(info.permissions).map(([key, value]) => (
+                <div key={key} className="flex flex-col gap-y-1">
+                  <p className="font-bold">{key}</p>
+                  <ul className="list-inside list-disc">
+                    {value.map((perm) => (
+                      <li key={perm}>{perm}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </AccordionContent>
+        )}
       </AccordionItem>
     </Accordion>
   );
--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,10 +1,10 @@
-import * as React from "react"
-import * as AccordionPrimitive from "@radix-ui/react-accordion"
-import { ChevronDownIcon } from "@radix-ui/react-icons"
+import * as React from "react";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const Accordion = AccordionPrimitive.Root
+const Accordion = AccordionPrimitive.Root;
 
 const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
@@ -15,28 +15,32 @@ const AccordionItem = React.forwardRef<
     className={cn("border-b", className)}
     {...props}
   />
-))
-AccordionItem.displayName = "AccordionItem"
+));
+AccordionItem.displayName = "AccordionItem";
 
 const AccordionTrigger = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger> & {
+    isPermPresent?: boolean;
+  }
+>(({ className, children, isPermPresent, ...props }, ref) => (
   <AccordionPrimitive.Header className="flex">
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
         "flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
-        className
+        className,
       )}
       {...props}
     >
       {children}
-      <ChevronDownIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      {isPermPresent && (
+        <ChevronDownIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      )}
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
-))
-AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
+));
+AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
 
 const AccordionContent = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Content>,
@@ -49,7 +53,7 @@ const AccordionContent = React.forwardRef<
   >
     <div className={cn("pb-4 pt-0", className)}>{children}</div>
   </AccordionPrimitive.Content>
-))
-AccordionContent.displayName = AccordionPrimitive.Content.displayName
+));
+AccordionContent.displayName = AccordionPrimitive.Content.displayName;
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
