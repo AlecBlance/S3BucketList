918e5513a80c5d1c4ab8437018915337fa40c36f
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2025-04-30 20:17:38 +0800

    chore: :fire: clear

--- src/store/useBuckets.store.ts
+++
@@ -1,9 +0,0 @@
-import { IUseBuckets } from "@/types";
-import { create } from "zustand";
-
-const useBuckets = create<IUseBuckets>((set) => ({
-  buckets: { good: [], bad: [], error: [] },
-  setBuckets: (buckets) => set({ buckets }),
-}));
-
-export default useBuckets;
