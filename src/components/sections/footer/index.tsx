218012fef74e0f29d9d1fd8b454100db5c1cde50
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2025-06-05 20:21:50 +0800

    feat(settings): :sparkles: add rate and help links

--- src/components/sections/footer/index.tsx
+++ src/components/sections/footer/index.tsx
@@ -1,14 +1,30 @@
-import { CiSettings } from "react-icons/ci";
 import Settings from "./Settings/Settings";
 
 const Footer = () => {
   return (
     <div className="bg-secondary flex items-center justify-between p-2">
-      <Settings />
+      <div className="flex items-center gap-4">
+        <Settings />
+        <a
+          href="https://github.com/AlecBlance/S3BucketList/issues"
+          target="_blank"
+          className="decoration-primary hover:underline"
+        >
+          ❓ Help
+        </a>
+        <a
+          href="https://chromewebstore.google.com/detail/s3bucketlist/anngjobjhcbancaaogmlcffohpmcniki/reviews"
+          target="_blank"
+          className="decoration-primary hover:underline"
+        >
+          ⭐ Rate
+        </a>
+      </div>
       <p>
         Made with ❤️ by{" "}
         <a
           href="https://alecblance.com"
+          target="_blank"
           className="decoration-primary hover:text-primary underline decoration-2 underline-offset-2"
         >
           Alec Blance
