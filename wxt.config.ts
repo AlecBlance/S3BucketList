bd52e1cfffd129a1f38eac2700e84264e8342ee7
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2025-08-09 20:38:42 +0800

    fix: :bug: build fail on axios ignoring ssl issues

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "wxt";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import fs from "fs";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -18,7 +19,7 @@ export default defineConfig({
     ).version;
 
     return {
-      plugins: [tailwindcss()],
+      plugins: [tailwindcss(), nodePolyfills()],
       resolve: {
         alias: {
           "@": path.resolve(__dirname, "./src"),
